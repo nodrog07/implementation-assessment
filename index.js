c6c1efb4e0e7cd3352af634300d0becb6ddaa9e8
nodrog07 <94332988+nodrog07@users.noreply.github.com>
2021-11-21 12:53:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ config.merchantAccount = MERCHANT_ACCOUNT;
 const client = new Client({ config });
 client.setEnvironment(ENVIRONMENT);
 const checkout = new CheckoutAPI(client);
-
+checkout.payments({
+    amount: { currency: "EUR", value: 1000 },
+    paymentMethod: {
+        type: 'sepadirectdebit',
+        sepa.ownerName: 'A. Schneider',
+        sepa.ibanNumber: 'DE87123456781234567890'
+    },
+    reference: "YOUR_ORDER_NUMBER",
+    merchantAccount: config.merchantAccount
+}).then(res => res);
 const app = express();
 app.use(express.json());
 
@@ -77,21 +86,6 @@ app.post('/additionalDetails', async (req, res) => {
     merchantreference,
     order,
     paymentmethod,
-    config.apiKey = '[API_KEY]';
-config.merchantAccount = '[YOUR_MERCHANT_ACCOUNT]';
-const client = new Client({ config });
-client.setEnvironment("TEST");
-const checkout = new CheckoutAPI(client);
-checkout.payments({
-    amount: { currency: "EUR", value: 1000 },
-    paymentMethod: {
-        type: 'sepadirectdebit',
-        sepa.ownerName: 'A. Schneider',
-        sepa.ibanNumber: 'DE87123456781234567890'
-    },
-    reference: "YOUR_ORDER_NUMBER",
-    merchantAccount: config.merchantAccount
-}).then(res => res);
     pspreference,
     refusalreason,
     refusalreasoncode,
