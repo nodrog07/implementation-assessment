310f9fdb7bc653c3db07afcc5362d0fd55452872
nodrog07 <94332988+nodrog07@users.noreply.github.com>
2021-11-21 12:10:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static(__dirname + '/public'));
 
 // this endpoint is (almost!) working
 app.post('/getPaymentMethods', (req, res) => {
-  const { AdyenRecruitment_NY1, countryCode, shopperLocale, amount } = req.body;
+  const { apikey, countryCode, shopperLocale, amount } = req.body;
   checkout.paymentMethods({
     merchantAccount,
     countryCode,
@@ -47,7 +47,7 @@ app.post('/getPaymentMethods', (req, res) => {
 
 // build this endpoint using the example above, along with our dropin documentation -> https://docs.adyen.com/online-payments/web-drop-in/integrated-before-5-0-0?tab=codeBlockmethods_request_7#step-3-make-a-payment
 app.post('/makePayment', (req, res) => {
-    const { AdyenRecruitment_NY1, amount, reference,paymentmethod, returnurl } = req.body;
+    const { apikey, amount, reference,paymentmethod, returnurl } = req.body;
   checkout.paymentMethods({
     merchantAccount,
     amount: {
@@ -66,7 +66,7 @@ app.post('/makePayment', (req, res) => {
 
 // build this endpoint as well, using the documentation -> https://docs.adyen.com/online-payments/web-drop-in/integrated-before-5-0-0?tab=codeBlockmethods_request_7#step-5-additional-payment-details
 app.post('/additionalDetails', async (req, res) => {
-  const { amount, donationtoken, fraudresult, merchantreference, order, paymentmethod, pspreference, refusalreason, refusalreasoncode, resultcode, shopperlocale, threeDS2ResponseData, threeDS2result, threeDSpaymentdata  } = req.body;
+  const { apikey, amount, donationtoken, fraudresult, merchantreference, order, paymentmethod, pspreference, refusalreason, refusalreasoncode, resultcode, shopperlocale, threeDS2ResponseData, threeDS2result, threeDSpaymentdata  } = req.body;
   checkout.paymentMethods({
     amount: {
       currency: amount.currency,
